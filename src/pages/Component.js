93a73aefe38b5bc63ac430684b0d8518ff467a31
hritikritesh01 <hritikritesh.38@gmail.com>
2023-04-08 14:27:17 +0530

    first commit

---
+++ src/pages/Component.js
@@ -0,0 +1,460 @@
+import "./Component.css";
+
+const Component = () => {
+  return (
+    <div className="div">
+      <div className="div-parent">
+        <div className="div1">
+          <div className="x-your-lead">5x Your Lead Flow</div>
+        </div>
+        <div className="div2">
+          <img className="image-icon" alt="" src="/image@2x.png" />
+        </div>
+        <div className="lorem-ipsum-dolor">
+          Lorem ipsum dolor sit amet consectetur. Risus sit vel sit in quisque
+          neque. Elit amet ut pellentesque purus morbi fringilla. Urna morbi dui
+          euismod curabitur ullamcorper aenean tempus nibh.
+        </div>
+      </div>
+      <div className="div3" />
+      <div className="action-parent">
+        <div className="action">
+          <b className="dashboard">Dashboard</b>
+          <img
+            className="vuesaxlineargrid-3-icon"
+            alt=""
+            src="/vuesaxlineargrid3.svg"
+          />
+        </div>
+        <b className="realply">Realply</b>
+        <div className="action1">
+          <b className="dashboard">Messages</b>
+          <img
+            className="vuesaxlinearmessage-icon"
+            alt=""
+            src="/vuesaxlinearmessage.svg"
+          />
+        </div>
+        <div className="action2">
+          <b className="calendar">Calendar</b>
+          <img
+            className="vuesaxlineargrid-3-icon"
+            alt=""
+            src="/vuesaxlinearcalculator.svg"
+          />
+        </div>
+        <div className="action3">
+          <b className="calendar">Campaigns</b>
+          <img
+            className="vuesaxlineargrid-3-icon"
+            alt=""
+            src="/vuesaxlinearflash.svg"
+          />
+        </div>
+        <div className="action4">
+          <b className="calendar">Settings</b>
+          <img
+            className="vuesaxlineargrid-3-icon"
+            alt=""
+            src="/vuesaxlinearsetting3.svg"
+          />
+        </div>
+        <div className="action5">
+          <b className="calendar">Help?</b>
+          <img
+            className="vuesaxlineargrid-3-icon"
+            alt=""
+            src="/vuesaxlineardanger.svg"
+          />
+        </div>
+        <div className="action6">
+          <b className="log-out">Log out</b>
+          <img
+            className="vuesaxlinearsend-square-icon"
+            alt=""
+            src="/vuesaxlinearsendsquare.svg"
+          />
+        </div>
+        <img className="frame-child" alt="" src="/group-185.svg" />
+        <div className="frame-item" />
+        <div className="frame-parent">
+          <div className="group-parent">
+            <div className="your-free-trial-is-about-to-en-wrapper">
+              <div className="your-free-trial-container">
+                <span className="your-free-trial-container1">
+                  <p className="your-free-trial-is-about-to-en">
+                    <span>Your free trial is about to end in</span>
+                    <span className="span">{` `}</span>
+                  </p>
+                  <p className="days">10 days.</p>
+                </span>
+              </div>
+            </div>
+            <div className="you-will-not-container">
+              <span className="your-free-trial-container1">
+                <p className="your-free-trial-is-about-to-en">
+                  You will not be billed during your free trial.
+                </p>
+                <p className="your-free-trial-is-about-to-en">&nbsp;</p>
+                <p className="your-free-trial-is-about-to-en">
+                  To keep your projects running after the trial end, upgrade to
+                  a paid option.
+                </p>
+              </span>
+            </div>
+          </div>
+          <div className="a-wrapper">
+            <div className="a">
+              <b className="upgrade">Upgrade</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="dashboard-parent">
+        <b className="dashboard1">Dashboard</b>
+        <div className="connections-parent">
+          <b className="connections">Connections</b>
+          <div className="group-child" />
+        </div>
+        <div className="follows-parent">
+          <b className="follows">Follows</b>
+          <div className="group-item" />
+        </div>
+        <div className="connect-invites-parent">
+          <b className="connect-invites">Connect Invites</b>
+          <div className="group-inner" />
+        </div>
+      </div>
+      <div className="vector-parent">
+        <img className="vector-icon" alt="" src="/vector-1.svg" />
+        <div className="group-container">
+          <div className="group-wrapper">
+            <div className="group-frame">
+              <div className="group-frame">
+                <div className="group-frame">
+                  <div className="div4">2632</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="followers">Connections</div>
+          <div className="parent">
+            <b className="b">56%</b>
+            <img
+              className="vuesaxlineartrend-up-icon"
+              alt=""
+              src="/vuesaxlineartrendup.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="group-parent1">
+        <div className="group-wrapper1">
+          <div className="container">
+            <div className="container">
+              <div className="container">
+                <div className="div5">286</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="invitations-sent">Invitations sent</div>
+      </div>
+      <div className="group-parent2">
+        <div className="group-wrapper4">
+          <div className="frame">
+            <div className="frame">
+              <div className="frame">
+                <div className="div6">12</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="invitations-sent">Pending Invitations</div>
+      </div>
+      <div className="group-parent3">
+        <div className="group-wrapper7">
+          <div className="group-wrapper8">
+            <div className="group-wrapper8">
+              <div className="group-wrapper8">
+                <div className="div7">2891</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="invitations-sent">Profile views</div>
+      </div>
+      <div className="vector-group">
+        <img className="vector-icon" alt="" src="/vector-1.svg" />
+        <div className="group-container">
+          <div className="group-wrapper10">
+            <div className="group-wrapper11">
+              <div className="group-wrapper11">
+                <div className="group-wrapper11">
+                  <div className="div8">2667</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="followers">Followers</div>
+          <div className="parent">
+            <b className="b1">0%</b>
+            <img
+              className="vuesaxlineartrend-up-icon"
+              alt=""
+              src="/vuesaxlineartrendup1.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="inner">
+        <div className="group-container">
+          <div className="group-wrapper13">
+            <div className="group-wrapper14">
+              <div className="group-wrapper14">
+                <div className="group-wrapper14">
+                  <div className="div9">100</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="followers">Connect Invites</div>
+          <div className="parent">
+            <b className="b2">60%</b>
+            <img
+              className="vuesaxlineartrend-up-icon"
+              alt=""
+              src="/vuesaxlineartrendup2.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="child">
+        <div className="line-parent">
+          <div className="line-div" />
+          <div className="group-child2" />
+          <div className="group-child3" />
+          <div className="group-child4" />
+          <div className="group-child5" />
+          <div className="group-child6" />
+          <div className="group-child7" />
+          <div className="group-child8" />
+          <div className="group-child9" />
+          <div className="rectangle-div" />
+          <div className="group-child10" />
+          <div className="group-child11" />
+          <div className="div10">0</div>
+          <div className="div11">100</div>
+          <div className="div12">200</div>
+          <div className="div13">300</div>
+          <div className="div14">400</div>
+          <div className="div15">500</div>
+          <div className="div16">600</div>
+          <div className="div17">700</div>
+          <div className="div18">800</div>
+          <div className="group-child12" />
+        </div>
+      </div>
+      <div className="recent-activity-parent">
+        <b className="recent-activity">Recent Activity</b>
+        <div className="alex-morgan-sent-container">
+          <b>{`Alex Morgan `}</b>
+          <span>sent you a</span>
+          <b> message.</b>
+        </div>
+        <div className="mujo-prosper-sent-container">
+          <b>{`Mujo Prosper `}</b>
+          <span>sent you a</span>
+          <b> connection request.</b>
+        </div>
+        <div className="upcoming-task-container">
+          <span>Upcoming Task</span>
+          <b>{` - Create a campaign for designers `}</b>
+          <span>due on</span>
+          <b> 02/04/2023.</b>
+        </div>
+        <img className="frame-inner" alt="" src="/ellipse-5@2x.png" />
+        <img className="ellipse-icon" alt="" src="/ellipse-6@2x.png" />
+        <img className="frame-child1" alt="" src="/ellipse-7.svg" />
+        <img
+          className="vuesaxlinear3square-icon"
+          alt=""
+          src="/vuesaxlinear3square.svg"
+        />
+        <img className="frame-child2" alt="" src="/vector-11.svg" />
+        <img className="frame-child3" alt="" src="/vector-11.svg" />
+        <div className="h-parent">
+          <b className="h">24h</b>
+          <div className="last">Last</div>
+          <img
+            className="vuesaxlineararrow-square-dow-icon"
+            alt=""
+            src="/vuesaxlineararrowsquaredown.svg"
+          />
+        </div>
+        <div className="scrollbar" />
+      </div>
+      <b className="campaign-analytics">Campaign Analytics</b>
+      <div className="group-parent6">
+        <div className="jacob-frost-wrapper">
+          <b className="jacob-frost">Jacob Frost</b>
+        </div>
+        <img className="group-child13" alt="" src="/ellipse-4@2x.png" />
+        <img
+          className="vuesaxlineararrow-square-dow-icon1"
+          alt=""
+          src="/vuesaxlineararrowsquaredown1.svg"
+        />
+      </div>
+      <img className="item" alt="" src="/group-34146.svg" />
+      <div className="chart-parent">
+        <div className="chart">
+          <img className="polygon-01" alt="" src="/polygon--01.svg" />
+          <img className="polygon-02" alt="" src="/polygon--02.svg" />
+          <img className="polygon-03" alt="" src="/polygon--03.svg" />
+          <img className="polygon-04" alt="" src="/polygon--04.svg" />
+          <img className="polygon-05" alt="" src="/polygon--05.svg" />
+          <img className="polygon-06" alt="" src="/polygon--06.svg" />
+          <img className="polygon-07" alt="" src="/polygon--07.svg" />
+          <img className="polygon-08" alt="" src="/polygon--08.svg" />
+          <img className="chart-child" alt="" src="/vector-15.svg" />
+          <div className="april">April</div>
+          <div className="may">May</div>
+          <div className="june">June</div>
+          <div className="july">July</div>
+          <div className="january">January</div>
+          <div className="february">February</div>
+          <div className="march">March</div>
+          <div className="number-01">
+            <div className="div19">10</div>
+          </div>
+          <div className="number-02">
+            <div className="div19">20</div>
+          </div>
+          <div className="number-03">
+            <div className="div19">30</div>
+          </div>
+          <div className="number-04">
+            <div className="div19">40</div>
+          </div>
+          <div className="number-05">
+            <div className="div19">50</div>
+          </div>
+          <div className="number-06">
+            <div className="div19">60</div>
+          </div>
+          <div className="number-07">
+            <div className="div19">70</div>
+          </div>
+          <div className="number-08">
+            <div className="div19">80</div>
+          </div>
+          <div className="number-09">
+            <div className="div19">90</div>
+          </div>
+          <img className="icon" alt="" src="/01.svg" />
+          <img className="bullet-icon" alt="" src="/bullet.svg" />
+          <img className="bullet-icon1" alt="" src="/bullet1.svg" />
+          <img className="bullet-icon2" alt="" src="/bullet2.svg" />
+          <img className="bullet-icon3" alt="" src="/bullet3.svg" />
+          <img className="bullet-icon4" alt="" src="/bullet4.svg" />
+          <img className="bullet-icon5" alt="" src="/bullet5.svg" />
+          <img className="bullet-icon6" alt="" src="/bullet6.svg" />
+          <img className="chart-item" alt="" src="/vector-16.svg" />
+          <img className="bullet-icon7" alt="" src="/bullet7.svg" />
+          <img className="bullet-icon8" alt="" src="/bullet8.svg" />
+          <img className="bullet-icon9" alt="" src="/bullet9.svg" />
+          <img className="bullet-icon10" alt="" src="/bullet10.svg" />
+          <img className="bullet-icon11" alt="" src="/bullet11.svg" />
+          <img className="bullet-icon12" alt="" src="/bullet12.svg" />
+          <img className="bullet-icon13" alt="" src="/bullet13.svg" />
+        </div>
+        <div className="info">
+          <div className="data-01">
+            <div className="bullet">
+              <div className="bullet-child" />
+            </div>
+            <div className="txt">
+              <div className="campaign-1">Campaign 1</div>
+              <div className="data">
+                <div className="campaign-1">12,423</div>
+              </div>
+            </div>
+          </div>
+          <div className="data-02">
+            <div className="bullet">
+              <div className="bullet-item" />
+            </div>
+            <div className="txt">
+              <div className="campaign-1">Campaign 2</div>
+              <div className="data">
+                <div className="campaign-1">12,423</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="doughnut-parent">
+        <img className="doughnut-icon" alt="" src="/doughnut.svg" />
+        <div className="info1">
+          <div className="data-011">
+            <div className="bullet">
+              <div className="bullet-inner" />
+            </div>
+            <div className="txt">
+              <div className="campaign-1">Invitations sent</div>
+              <div className="data">
+                <div className="campaign-1">286</div>
+              </div>
+            </div>
+          </div>
+          <div className="data-021">
+            <div className="bullet">
+              <div className="bullet-child1" />
+            </div>
+            <div className="txt">
+              <div className="campaign-1">Pending invitations</div>
+              <div className="data">
+                <div className="campaign-1">12</div>
+              </div>
+            </div>
+          </div>
+          <div className="data-03">
+            <div className="bullet">
+              <div className="bullet-child2" />
+            </div>
+            <div className="txt">
+              <div className="campaign-1">Profile views</div>
+              <div className="data">
+                <div className="campaign-1">2891</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mar-10-apr-10-parent">
+        <div className="mar-10-">Mar 10 - Apr 10</div>
+        <img
+          className="vuesaxlineararrow-square-dow-icon2"
+          alt=""
+          src="/vuesaxlineararrowsquaredown2.svg"
+        />
+        <img
+          className="vuesaxlinearcalculator-icon1"
+          alt=""
+          src="/vuesaxlinearcalculator1.svg"
+        />
+      </div>
+      <div className="search-parent">
+        <div className="search">Search</div>
+        <img
+          className="vuesaxlinearsearch-normal-icon"
+          alt=""
+          src="/vuesaxlinearsearchnormal.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Component;
